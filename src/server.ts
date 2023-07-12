8bc4f3687b0e6b13d9b6fe573c5f22b02eac20c3
Pedro Rosa <pedrorosadomingues@gmail.com>
2023-07-12 15:35:09 -0300

    feat: iniciate project

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import app, { init } from './index';
+
+const port = process.env.PORT || 5000;
+
+init().then(() => {
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}.`);
+  });
+});
